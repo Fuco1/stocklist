e619570342e81a0635d11b90014cd770c6a5aa8e
Matus Goljer <matus.goljer@gmail.com>
2016-03-12 19:19:00 +0100

    Add query language for selecting instruments.

--- stocklist.el
+++ stocklist.el
@@ -56,11 +56,31 @@ Each instrument has some optional properties:
                                              (:tags (repeat string)))))
   :group 'stocklist)
 
-(defun stocklist-instruments ()
+(defun stocklist-instruments (&optional query)
   "Return the tracked instruments.
 
-This extracts the symbols from `stocklist-instruments' variable."
-  (-map 'car stocklist-instruments))
+QUERY is a query to select instruments by tags.  You can join
+multiple tags with + to select instruments with all the tags.
+You can join multiple tags with | to select tags with at least
+one of the tags in the group.  You can prefix a tag with ! to
+select instruments without this tag.  Logical and (+) has highest
+priority.
+
+Examples:
+  a+b   : select all instruments with both tags \"a\" and \"b\"
+  a|b   : select all instruments with either tags \"a\" or \"b\"
+  a+b|c : select all instruments with tag \"a\" and either \"b\" or \"c\"
+  !a    : select all instruments without the tag \"a\""
+  (setq query (or query ""))
+  (let* ((and-groups (split-string query "+" t))
+         (or-groups (--map (split-string it "|") and-groups))
+         (form `(and ,@(-map
+                        (lambda (g)
+                          `(or ,@(--map (if (= (aref it 0) ?!)
+                                            `(not (member ,(substring it 1) tags))
+                                          `(member ,it tags)) g)))
+                        or-groups))))
+    (-map 'car (--filter (eval `(let ((tags ',(plist-get (cdr it) :tags))) ,form)) stocklist-instruments))))
 
 (defcustom stocklist-quandl-api-key "74j1sBFqMF1_hsKgSC8x"
   "API Key for Quandl database."
